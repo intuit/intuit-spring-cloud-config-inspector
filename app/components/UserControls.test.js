74e8776e88e4d6ea8667b2f8a31953611bf2c338
Alexi Stein <Alexi_Stein@intuit.com>
2017-07-26 12:29:55 -0700

    CFG-773: Added a test for rendering Headers and updated two more tests

--- app/components/UserControls.test.js
+++ app/components/UserControls.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import UserControls from './UserControls.jsx';
 
 it('renders without crashing', () => {
+  const inputData = {'url': 'https://config.api.intuit.com/v2', 'app': '{app}', 'profiles': 'default', 'label': 'master'}
   const div = document.createElement('div');
-  ReactDOM.render(<UserControls />, div);
+  ReactDOM.render(<UserControls inputData={inputData} />, div);
 });
