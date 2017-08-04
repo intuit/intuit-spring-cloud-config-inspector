c53500928f864f90fcaf51f65bdba5927bf4164b
Alexi Stein <Alexi_Stein@intuit.com>
2017-08-03 17:09:14 -0700

    Removed unused variable

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -92,7 +92,7 @@ export default class App extends React.Component {
 
   render() {
     const { header, headerCount, inputData, urls } = this.state
-    const inputDataClone = {...inputData}
+    
     return (
       <div>
         <ReactCSSTransitionGroup
