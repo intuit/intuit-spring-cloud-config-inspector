f4a0f782eaf046f70cd12b0c2a96dd99b155c6bf
Alexi Stein <Alexi_Stein@intuit.com>
2017-08-14 16:49:06 -0700

    CFG 778: Filterable properties, no longer accept additions because it doesn't make sense, fixed bug in LabelMenu (tags not selectable)

--- app/components/PropSearch.jsx
+++ app/components/PropSearch.jsx
@@ -4,13 +4,6 @@ import PropTypes from 'prop-types'
 
 import {Dropdown, Input} from 'semantic-ui-react'
 
-const source = _.times(10, (index) => ({
-  key: index,
-  text: `property ${index}`,
-  value: `property ${index}`,
-  description: `description ${index}`
-}))
-
 export default class PropSearch extends React.Component {
 
   static propTypes = {
@@ -19,38 +12,17 @@ export default class PropSearch extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {source}
   }
 
   /**
-   * When user adds a profile that does not exist in search
-   * results, add label 'Not found' to new input
+   * When the user adds or removes a property, update parent Views component.
    *
    * @param {SyntheticEvent} e - React's original SyntheticEvent.
    * @param {object} props
    * @param {string} props.value - current input
    */
-  handleAddition = (e, {value}) => {
-    let label = { color:'red', content:'Not found' }
-    this.setState({
-      source: [{text:value, value, label}, ...this.state.source]
-    })
-  }
-
-  /**
-   * item.label returns true if the item (property input) was
-   * an addition. If true add label to option in Dropdown menu.
-   *
-   * @param {object} item - A currently active dropdown item.
-   * @param {number} index - The current index.
-   * @param {object} props - The default props for an active item Label.
-   * @returns Shorthand for a Label.
-   */
-  renderLabel = (item, index, props) => {
-    if (item.label) {
-      return {color:'red', content:`Not found: ${item.text}`}
-    }
-    return {content:item.text}
+  handleChange = (e, {value}) => {
+    this.props.updateFilter(value)
   }
 
   render() {
@@ -62,9 +34,7 @@ export default class PropSearch extends React.Component {
         icon='search'
         placeholder='properties...'
         options={options.map(key => ({key:key, text:key, value:key}))}
-        allowAdditions additionLabel='Custom: '
-        onAddItem={this.handleAddition}
-        renderLabel={this.renderLabel}
+        onChange={this.handleChange}
       />
     )
   }
