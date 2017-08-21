af4dfb9a4877134a3767cde61bc5ba35ac72e73f
Marcello de Sales <Marcello_deSales@intuit.com>
2017-08-21 08:49:59 -0700

    CFG-876: Removing API keys not used

--- app/conf.js
+++ app/conf.js
@@ -2,11 +2,6 @@ const packageJson = require("../package.json");
 
 export const GIT_REPOS_API = 'https://github.intuit.com/api/v3/repos'
 
-export const API_KEYS = {
-  prod: 'prdakyresk43bOEZ0adQWBF3hRw8vfAOxq3rWPXg',
-  preProd: 'preprdakyresGHM824zIZ1pQ2jdP7VbRsCWGKSzC'
-};
-
 /**
  * Current Credentials
  */
